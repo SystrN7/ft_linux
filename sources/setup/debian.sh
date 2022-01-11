4b2e0a2723b390e47b85bae80da04297660337bc
Felix Galaup <felixgalaup@yahoo.fr>
2022-01-11 10:39:32 +0100

    [^]-Fix setup filename + add lfs deps.

---
+++ sources/setup/debian.sh
@@ -0,0 +1,27 @@
+sudo apt install -y
+    build-essential
+    wget \
+    bash \
+    binutils \
+    bison \
+    bzip2 \
+    coreutils \
+    diffutils \
+    findutils \
+    gawk \
+    gcc \
+    grep \
+    gzip \
+    m4 \
+    make \
+    patch \
+    perl \
+    python3 \
+    sed \
+    tar \
+    texinfo \
+    xz-utils
+
+
+sudo rm /bin/sh
+sudo ln -s /usr/bin/bash /bin/sh
