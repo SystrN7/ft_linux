1ddccf992783405e52b8d5594172c64c15229509
SystrN7 <felixgalaup@yahoo.fr>
2022-09-27 14:31:16 +0200

    [^]- Fix tools link

--- sources/steps/user-env/create-tools-directory.sh
+++ sources/steps/user-env/create-tools-directory.sh
@@ -6,7 +6,7 @@
 #    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/02 12:55:10 by felix             #+#    #+#              #
-#    Updated: 2022/09/16 19:19:25 by felix            ###   ########lyon.fr    #
+#    Updated: 2022/09/27 14:12:26 by felix            ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -16,8 +16,8 @@
 mkdir $LFS_TOOLS_PATH
 mkdir $LFS_SOURCES_PATH
 
-# Create link between host system an lfs
-sudo ln -s $LFS_TOOLS_PATH /$LFS_TOOLS_DIRECTORY
+# Create link between host system and lfs
+sudo ln -s $(pwd)$LFS_TOOLS_PATH /$LFS_TOOLS_DIRECTORY
 
 # Change tools give permision
 sudo chown -v $LFS_USER $LFS_TOOLS_PATH
