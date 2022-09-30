c2ec72c53bc17573c9a4ea27266e65c48dfcbc13
SystrN7 <felixgalaup@yahoo.fr>
2022-09-30 15:06:17 +0200

    [+]-Add m4 build

--- sources/steps/build-toolchain.sh
+++ sources/steps/build-toolchain.sh
@@ -6,7 +6,7 @@
 #    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/13 22:19:37 by felix             #+#    #+#              #
-#    Updated: 2022/09/29 19:31:48 by felix            ###   ########lyon.fr    #
+#    Updated: 2022/09/30 14:57:41 by felix            ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -66,4 +66,7 @@ mkdir -v $LFS_TOOLS_PATH/build/
 # source ./sources/steps/build/toolchain/expect.sh
 
 # Build DejaGNU (Test) (Optional)
-source ./sources/steps/build/toolchain/dejagnu.sh
+# source ./sources/steps/build/toolchain/dejagnu.sh
+
+# Build M4
+source ./sources/steps/build/toolchain/m4.sh
---
+++ sources/steps/build/toolchain/m4.sh
@@ -0,0 +1,37 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    m4.sh                                              :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/09/30 14:58:10 by felix             #+#    #+#              #
+#    Updated: 2022/09/30 15:01:40 by felix            ###   ########lyon.fr    #
+#                                                                              #
+# **************************************************************************** #
+
+# Copy sources.
+cp -r --preserve build/lfs/sources/m4-1.4.18 $LFS_TOOLS_PATH/build/
+
+pushd $LFS_TOOLS_PATH/build/m4-1.4.18
+
+# Applies fix to sources
+sed -i 's/IO_ftrylockfile/IO_EOF_SEEN/' lib/*.c
+echo "#define _IO_IN_BACKUP 0x100" >> lib/stdio-impl.h
+
+# Create makefile with config
+./configure --prefix=/tools
+
+# Build
+make
+
+# Run test
+make check
+
+# Install
+make install
+
+popd
+
+# Remove temps build dir
+rm -rf $LFS_TOOLS_PATH/build/m4-1.4.18
