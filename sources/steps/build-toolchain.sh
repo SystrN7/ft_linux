e38b0115f367019d7bd622b0ca9910f8566550f6
SystrN7 <felixgalaup@yahoo.fr>
2022-10-18 12:53:53 +0200

    Build all toolchain.

--- sources/steps/build-toolchain.sh
+++ sources/steps/build-toolchain.sh
@@ -6,7 +6,7 @@
 #    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/09/13 22:19:37 by felix             #+#    #+#              #
-#    Updated: 2022/10/18 10:11:30 by felix            ###   ########lyon.fr    #
+#    Updated: 2022/10/18 12:48:36 by felix            ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -33,103 +33,103 @@ export LFS_PATH=$(pwd)/$LFS_PATH;
 mkdir -vp $LFS_TOOLS_PATH/build/
 
 # Build binutils (Passe 1)
-# source ./sources/steps/build/toolchain/binutils.sh
+source ./sources/steps/build/toolchain/binutils.sh
 
 # Build GCC (Passe 1)
-# source ./sources/steps/build/toolchain/gcc.sh
+source ./sources/steps/build/toolchain/gcc.sh
 
 # Build (Extract linux header)
-# source ./sources/steps/build/toolchain/linux-headers.sh
+source ./sources/steps/build/toolchain/linux-headers.sh
 
 # Build Glibc
-# source ./sources/steps/build/toolchain/glibc.sh
+source ./sources/steps/build/toolchain/glibc.sh
 
 # Test the current stage of build
-# source ./sources/steps/build/tests/toolchain-glibc-test.sh
+source ./sources/steps/build/tests/toolchain-glibc-test.sh
 
 # Build Standard library C++
-# source ./sources/steps/build/toolchain/libstdcpp.sh
+source ./sources/steps/build/toolchain/libstdcpp.sh
 
 # Build binutils (Passe 2)
-# source ./sources/steps/build/toolchain/binutils-2.sh
+source ./sources/steps/build/toolchain/binutils-2.sh
 
 # Build GCC (Passe 2)
-# source ./sources/steps/build/toolchain/gcc-2.sh
+source ./sources/steps/build/toolchain/gcc-2.sh
 
 # Test the current stage of build
-# source ./sources/steps/build/tests/toolchain-glibc-test.sh
+source ./sources/steps/build/tests/toolchain-glibc-test.sh
 
 # Build TCL (Test) (Optional)
-# source ./sources/steps/build/toolchain/tcl.sh
+source ./sources/steps/build/toolchain/tcl.sh
 
 # Build expect (Test) (Optional)
-# source ./sources/steps/build/toolchain/expect.sh
+source ./sources/steps/build/toolchain/expect.sh
 
 # Build DejaGNU (Test) (Optional)
-# source ./sources/steps/build/toolchain/dejagnu.sh
+source ./sources/steps/build/toolchain/dejagnu.sh
 
 # Build M4
-# source ./sources/steps/build/toolchain/m4.sh
+source ./sources/steps/build/toolchain/m4.sh
 
 # Build Ncurses
-# source ./sources/steps/build/toolchain/ncurses.sh
+source ./sources/steps/build/toolchain/ncurses.sh
 
 # Build Bash
-# source ./sources/steps/build/toolchain/bash.sh
+source ./sources/steps/build/toolchain/bash.sh
 
 # Build Bison
-# source ./sources/steps/build/toolchain/bison.sh
+source ./sources/steps/build/toolchain/bison.sh
 
 # Build Bzip2
-# source ./sources/steps/build/toolchain/bzip2.sh
+source ./sources/steps/build/toolchain/bzip2.sh
 
 # Build CoreUtils
-# source ./sources/steps/build/toolchain/coreutils.sh
+source ./sources/steps/build/toolchain/coreutils.sh
 
 # Build DiffUtils
-# source ./sources/steps/build/toolchain/diffutils.sh
+source ./sources/steps/build/toolchain/diffutils.sh
 
 # Build File
-# source ./sources/steps/build/toolchain/file.sh
+source ./sources/steps/build/toolchain/file.sh
 
 # Build FindUtils
-# source ./sources/steps/build/toolchain/findutils.sh
+source ./sources/steps/build/toolchain/findutils.sh
 
 # Build Gawk
-# source ./sources/steps/build/toolchain/gawk.sh
+source ./sources/steps/build/toolchain/gawk.sh
 
 # Build GetText
-# source ./sources/steps/build/toolchain/gettext.sh
+source ./sources/steps/build/toolchain/gettext.sh
 
 # Build Grep
-# source ./sources/steps/build/toolchain/grep.sh
+source ./sources/steps/build/toolchain/grep.sh
 
 # Build Gzip
-# source ./sources/steps/build/toolchain/gzip.sh
+source ./sources/steps/build/toolchain/gzip.sh
 
 # Build Make
-# source ./sources/steps/build/toolchain/make.sh
+source ./sources/steps/build/toolchain/make.sh
 
 # Build Patch
-# source ./sources/steps/build/toolchain/patch.sh
+source ./sources/steps/build/toolchain/patch.sh
 
 # Build Perl
-# source ./sources/steps/build/toolchain/perl.sh
+source ./sources/steps/build/toolchain/perl.sh
 
 # Build Python
-# source ./sources/steps/build/toolchain/python.sh
+source ./sources/steps/build/toolchain/python.sh
 
 # Build Sed
-# source ./sources/steps/build/toolchain/sed.sh
+source ./sources/steps/build/toolchain/sed.sh
 
 # Build Tar
-# source ./sources/steps/build/toolchain/tar.sh
+source ./sources/steps/build/toolchain/tar.sh
 
 # Build textinfo
-# source ./sources/steps/build/toolchain/textinfo.sh
+source ./sources/steps/build/toolchain/textinfo.sh
 
 # Build Xz
-# source ./sources/steps/build/toolchain/xz.sh
+source ./sources/steps/build/toolchain/xz.sh
 
 # ================================================= #
 # Step 4.1 : Clean useless files of build   		#
