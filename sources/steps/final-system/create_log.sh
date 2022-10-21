2c5532c858aa5b6bcb46fcc9517a0c5450f577dd
SystrN7 <felixgalaup@yahoo.fr>
2022-10-21 15:13:20 +0200

    [+]-Create login log files

--- sources/steps/final-system/create_log.sh
+++ sources/steps/final-system/create_log.sh
@@ -1,28 +1,17 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    prepare-system.sh                                  :+:      :+:    :+:    #
+#    create_log.sh                                      :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/10/18 12:35:44 by felix             #+#    #+#              #
-#    Updated: 2022/10/21 15:10:49 by felix            ###   ########lyon.fr    #
+#    Created: 2022/10/18 12:30:04 by felix             #+#    #+#              #
+#    Updated: 2022/10/21 14:47:45 by felix            ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
-# Debug
-# env
-# pwd
-# ls /
-
-# Create base directory
-# source /script/final-system/create_root_directory.sh
-
-# Create link betwen final system and cross toolchain
-# source /script/final-system/create_symbolic_link.sh
-
-# Create user and groups
-# source /script/final-system/create_users_groups.sh
-
-# Create login logs
-source /script/final-system/create_log.sh
+# Create system logs files (for user authentification)
+touch /var/log/{btmp,lastlog,faillog,wtmp}
+chgrp -v utmp /var/log/lastlog
+chmod -v 664  /var/log/lastlog
+chmod -v 600  /var/log/btmp
