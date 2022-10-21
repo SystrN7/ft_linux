d33a57eadd7c3ea92612b79220fedd7563e8c802
SystrN7 <felixgalaup@yahoo.fr>
2022-10-21 14:56:29 +0200

    [+]-install croos toolchain to the final systen (temp install)

--- sources/steps/final-system/create_symbolic_link.sh
+++ sources/steps/final-system/create_symbolic_link.sh
@@ -1,22 +1,23 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    prepare-system.sh                                  :+:      :+:    :+:    #
+#    create_symbolic_link.sh                            :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/10/18 12:35:44 by felix             #+#    #+#              #
-#    Updated: 2022/10/21 14:53:53 by felix            ###   ########lyon.fr    #
+#    Created: 2022/10/18 11:33:46 by felix             #+#    #+#              #
+#    Updated: 2022/10/18 16:40:31 by felix            ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
-# Debug
-# env
-# pwd
-# ls /
+# Links required binarys
+ln -sv /tools/bin/{bash,cat,chmod,dd,echo,ln,mkdir,pwd,rm,stty,touch} /bin
+ln -sv /tools/bin/{env,install,perl,printf}         /usr/bin
+ln -sv /tools/lib/libgcc_s.so{,.1}                  /usr/lib
+ln -sv /tools/lib/libstdc++.{a,so{,.6}}             /usr/lib
 
-# Create base directory
-# source /script/final-system/create_root_directory.sh
+install -vdm755 /usr/lib/pkgconfig
 
-# Create link betwen final system and cross toolchain
-source /script/final-system/create_symbolic_link.sh 
+ln -sv bash /bin/sh
+
+ln -sv /proc/self/mounts /etc/mtab
