95846b0c60d496835f2d34107e0effab2d65a4fc
SystrN7 <felixgalaup@yahoo.fr>
2022-10-21 15:09:33 +0200

    [+]-Create users and groups

---
+++ sources/steps/final-system/create_users_groups.sh
@@ -0,0 +1,48 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    create_users_groups.sh                             :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/10/18 11:51:00 by felix             #+#    #+#              #
+#    Updated: 2022/10/18 11:51:59 by felix            ###   ########lyon.fr    #
+#                                                                              #
+# **************************************************************************** #
+
+# Create users
+cat > /etc/passwd << "EOF"
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/dev/null:/bin/false
+daemon:x:6:6:Daemon User:/dev/null:/bin/false
+messagebus:x:18:18:D-Bus Message Daemon User:/var/run/dbus:/bin/false
+nobody:x:99:99:Unprivileged User:/dev/null:/bin/false
+EOF
+
+# Create groups
+cat > /etc/group << "EOF"
+root:x:0:
+bin:x:1:daemon
+sys:x:2:
+kmem:x:3:
+tape:x:4:
+tty:x:5:
+daemon:x:6:
+floppy:x:7:
+disk:x:8:
+lp:x:9:
+dialout:x:10:
+audio:x:11:
+video:x:12:
+utmp:x:13:
+usb:x:14:
+cdrom:x:15:
+adm:x:16:
+messagebus:x:18:
+input:x:24:
+mail:x:34:
+kvm:x:61:
+wheel:x:97:
+nogroup:x:99:
+users:x:999:
+EOF
--- sources/steps/prepare-system.sh
+++ sources/steps/prepare-system.sh
@@ -6,7 +6,7 @@
 #    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/18 12:35:44 by felix             #+#    #+#              #
-#    Updated: 2022/10/21 14:53:53 by felix            ###   ########lyon.fr    #
+#    Updated: 2022/10/21 14:58:19 by felix            ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,4 +19,7 @@
 # source /script/final-system/create_root_directory.sh
 
 # Create link betwen final system and cross toolchain
-source /script/final-system/create_symbolic_link.sh 
+# source /script/final-system/create_symbolic_link.sh
+
+# Create user and groups
+source /script/final-system/create_users_groups.sh
