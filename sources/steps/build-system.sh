13ef181a0e31f2aa4e4823ee5fd63c441da56977
SystrN7 <felixgalaup@yahoo.fr>
2022-11-02 10:56:15 +0100

    [+][system]-Build & Install Make.

--- sources/steps/build-system.sh
+++ sources/steps/build-system.sh
@@ -6,7 +6,7 @@
 #    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/10/18 10:34:18 by felix             #+#    #+#              #
-#    Updated: 2022/11/02 10:50:52 by felix            ###   ########lyon.fr    #
+#    Updated: 2022/11/02 10:54:18 by felix            ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
@@ -193,4 +193,7 @@ mkdir -vp /build
 # source /script/build/kdb.sh
 
 # Build & Install LibPipeline
-source /script/build/libpipeline.sh
+# source /script/build/libpipeline.sh
+
+# Build & Install Make
+source /script/build/make.sh
---
+++ sources/steps/build/system/make.sh
@@ -0,0 +1,35 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    make.sh                                            :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/11/02 10:03:49 by felix             #+#    #+#              #
+#    Updated: 2022/11/02 10:53:37 by felix            ###   ########lyon.fr    #
+#                                                                              #
+# **************************************************************************** #
+
+# Copy sources.
+cp -r --preserve /sources/make-4.2.1 /build/make-4.2.1
+
+pushd /build/make-4.2.1
+
+# Fix Glibc 2.27 error
+sed -i '211,217 d; 219,229 d; 232 d' glob/glob.c
+
+# Create makefile with config
+./configure --prefix=/usr
+
+# Build
+make
+
+# Run test
+make PERL5LIB=$PWD/tests/ check
+
+# Install
+make install
+
+popd
+
+rm -rf /build/make-4.2.1
