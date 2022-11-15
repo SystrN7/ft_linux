98cad683717cd50745050a1bbc530a3b75f6fb89
SystrN7 <felixgalaup@yahoo.fr>
2022-11-15 10:54:49 +0100

    [^]-Remove alias modification to prevent error.

--- sources/steps/user-env/create-program-aliases.sh
+++ sources/steps/user-env/create-program-aliases.sh
@@ -6,12 +6,13 @@
 #    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/04/02 16:43:33 by felix             #+#    #+#              #
-#    Updated: 2022/04/02 16:52:57 by felix            ###   ########lyon.fr    #
+#    Updated: 2022/11/10 17:30:59 by felix            ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/bash
 
-ln -s /usr/bin/bash /usr/bin/sh
+echo "Check if link is good"
+echo "ln -s /usr/bin/bash /usr/bin/sh
 ln -s /usr/bin/gawk /usr/bin/awk
-ln -s /usr/bin/bison /usr/bin/yacc
+ln -s /usr/bin/bison /usr/bin/yacc"
