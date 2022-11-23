be1f38712b39f4ceaef4a8d91f9d1495b0a4f3f2
SystrN7 <felixgalaup@yahoo.fr>
2022-11-23 15:08:23 +0100

    [+][System]-Add utils to convert img to vhd.

---
+++ sources/utils/convert-img-to-vhd.sh
@@ -0,0 +1,2 @@
+# 'C:/Program Files/Oracle/VirtualBox/VBoxManage.exe' convertfromraw linux_from_scratch.raw ft_linux.vhd --format VHD
+VBoxManage convertfromraw linux_from_scratch.raw ft_linux.vhd --format VHD
