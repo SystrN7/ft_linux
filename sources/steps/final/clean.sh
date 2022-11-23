f61e28c5389e9bec7b12c617a53b406208254cb7
SystrN7 <felixgalaup@yahoo.fr>
2022-11-23 19:30:25 +0100

    [+]-System clean.

--- sources/steps/final/clean.sh
+++ sources/steps/final/clean.sh
@@ -1,26 +1,18 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    final.sh                                           :+:      :+:    :+:    #
+#    clean.sh                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: felix <felix@student.42lyon.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2022/11/03 12:04:57 by felix             #+#    #+#              #
-#    Updated: 2022/11/23 19:11:27 by felix            ###   ########lyon.fr    #
+#    Created: 2022/11/23 16:00:02 by felix             #+#    #+#              #
+#    Updated: 2022/11/23 16:01:56 by felix            ###   ########lyon.fr    #
 #                                                                              #
 # **************************************************************************** #
 
-# Make multi-hreaded build
-export MAKEFLAGS='-j '$(nproc --all)
-
-# Build & Install linux kernel
-source /script/build-linux.sh
-
-# Install Grub (boot loader)
-source /script/build-grub.sh
-
-# Clean System
-source /script/clean.sh
-
-# Clean everything
-rm -rf /script /tools
+# Remove scipt folder
+rm -rf /script
+# Remove build folder
+rm -rf /build
+# Remove tools folder
+rm -rf /tools
