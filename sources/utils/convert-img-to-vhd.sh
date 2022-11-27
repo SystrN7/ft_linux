37ba7b0870219032765b271b6e4d2ce0bd72a8a4
SystrN7 <felixgalaup@yahoo.fr>
2022-11-28 00:16:37 +0100

    [+]-Convert raw to vhd with qemu.

--- sources/utils/convert-img-to-vhd.sh
+++ sources/utils/convert-img-to-vhd.sh
@@ -1,2 +1,3 @@
 # 'C:/Program Files/Oracle/VirtualBox/VBoxManage.exe' convertfromraw linux_from_scratch.raw ft_linux.vhd --format VHD
-VBoxManage convertfromraw linux_from_scratch.raw ft_linux.vhd --format VHD
+VBoxManage convertfromraw linux_from_scratch.raw ft_linux.vhd --format VHD # With virtual box 
+qemu-img convert -f raw -O vpc build/linux_from_scratch.img  ft_linux.vhd # With qemu
